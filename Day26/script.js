b63bd0be665b185fa102121e9540c5e506a627fb
Kopal20 <kopalgangrade22@navgurukul.org>
2024-07-24 15:13:20 +0530

    Day26

---
+++ Day26/script.js
@@ -0,0 +1,5 @@
+const countries = document.querySelectorAll('.countires')
+
+
+
+fetch ("curl --location 'https://countriesnow.space/api/v0.1/countries/population/cities' \--data '{"city":"lagos"}'")
